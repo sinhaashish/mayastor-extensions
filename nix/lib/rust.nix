72195b6ec724a687708f7858fab740a3fb1f20f0
Tiago Castro <tiagolobocastro@gmail.com>
2024-11-04 23:37:08 +0000

    build: use correct pkg-config package
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- nix/lib/rust.nix
+++ nix/lib/rust.nix
@@ -62,7 +62,7 @@ rec {
     '';
     addPreBuild = "";
     nativeBuildInputs = with pkgs;
-      [ pkgconfig protobuf paperclip which git ] ++
+      [ pkg-config protobuf paperclip which git ] ++
         [ rustPlatformDeps.pkgsTarget.stdenv.cc ] ++
         lib.optional (rustPlatformDeps.pkgsTarget.hostPlatform.isDarwin)
           [
